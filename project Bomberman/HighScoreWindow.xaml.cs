fb4655f4f8198e60a62c5e6a05b7d16c703d1bf3
Marco Boschma <marcoboschma@live.nl>
2021-10-29 14:13:23 +0200

    Tile heeft comments

--- project Bomberman/HighScoreWindow.xaml.cs
+++ project Bomberman/HighScoreWindow.xaml.cs
@@ -24,105 +24,98 @@ namespace project_Bomberman
     /// 
     public partial class HighScoreWindow : Window 
     {
+        //maak een dictonary voor de highscore aan
         Dictionary<string, int> highscores = new Dictionary<string, int>();
-        const string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=D:\\Programmeren 1\\project Bomberman\\project Bomberman\\Data\\db_highscores.mdf;Integrated Security=True";
+        //een string die vervolgens het pad naar de database gaat bevatten
         string path = "";
+        //pak de locale pad naar database
         public void SetDir()
         {
+            //pak de actieve directory
             string workingDirectory = Environment.CurrentDirectory;
-
+            //de terugegeven map geeft een te hoog pad terug dus we gaan 2 stappen terug
             string dire = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            //zet de source naar het pad
             path = "Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename=" + dire + "\\Data\\db_highscores.mdf;Integrated Security = True";
-
         }
+        //Zet de highscore window klaar
         public HighScoreWindow()
         {
+            //zet alles klaar
             InitializeComponent();
+            //pak de locale pad naar database
             SetDir();
+            //Pak de highscores
             GetHighScores();
+            //maak tabellen aan voor de highscores
             CreateLabels();
         }
-
+        //pak alle opgeslagen scores uit de database
         private void GetHighScores()
         {
-            //todo get highscores
+            //maak de highscore disctonary leeg
             highscores.Clear();
-            for(int i = 0; i < highscores.Count - 1; i++)
-            {
-                //if(highscores.ElementAt(i).Value )
-            }
-
+            //de nodige query voor de database
             string query = "SELECT Name,Score FROM [Table];";
-
+            //gebruik een connection met het gegeven pad
             using (SqlConnection connection = new SqlConnection(path))
             {
+                //niew commdando met de gegeven info
                 SqlCommand command = new SqlCommand(query, connection);
+                //open verbinding met de database
                 connection.Open();
-
+                //maak een lokale reader class aan zodat we data uit kunnen lezen
                 SqlDataReader reader = command.ExecuteReader();
 
-                // Call Read before accessing data.
+                // Call lees voordat we de data gerbuiken
                 while (reader.Read())
                 {
-                    //MessageBox.Show("Data: " + reader[0] + " en: " + reader[1]);
-                    //highscores.Add((string)reader[0], (int)reader[1]);
+                    //als de gebruikersnaam al bestaad of de score nul of lager is
                     if (highscores.ContainsKey((string)reader[0]) || (int)reader[1] <= 0)
                     {
+                        //sla een stap over
                         continue;
                     }
                     else
                     {
+                        //voeg de data toe aan de databse
                         highscores.Add((string)reader[0], (int)reader[1]);
                     }
                 }
 
-                // Call Close when done reading.
+                // sluit de reader
                 reader.Close();
             }
         }
 
-        public void SetHighScores(string SQL_Text)
-        {
-
-            //string query1 = "INSERT INTO [Table] ([Player],[Score]) VALUES ('Jos','500')";
-
-            SqlConnection connection = new SqlConnection(path);
-
-            SqlCommand command = new SqlCommand();
-            try
-            {
-                command.CommandText = SQL_Text;
-                command.CommandType = CommandType.Text;
-                command.Connection = connection;
-                connection.Open();
-                command.ExecuteNonQuery();
-                connection.Close();
-                //MessageBox.Show("Gelukt!");
-            }
-            catch (Exception e)
-            {
-                connection.Close();
-                MessageBox.Show(e.Message);
-            }
-        }
-
+        //Maak niewe labels aan voor de score
         private void CreateLabels()
         {
+            //leeg het highscore paneel
             HighScoresPanel.Children.Clear();
+            //sorteer alle scores van hoog naar laag
             var sortedHighscores = from score in highscores orderby score.Value descending select score;
+            //voor elke key met een string en int in de sortedHighscores
             foreach (KeyValuePair<string, int> highscore in sortedHighscores)
             {
+                //maak een nieuwe label
                 Label label = new Label();
+                //voeg data toe aan de label
                 label.Content = "Player " + highscore.Key + " scored " + highscore.Value;
+                //verplaats de label
                 label.HorizontalAlignment = HorizontalAlignment.Center;
+                //voeg de nieuwe label toe aan het highscorePanel
                 HighScoresPanel.Children.Add(label);
             }
         }
-
+        //terugknop 
         private void backBtn(object sender, RoutedEventArgs e)
         {
+            //maak hoofdmenu aan
             Hoofdmenu h = new Hoofdmenu();
+            //maak hoofdmenu zichtbaar
             h.Visibility = Visibility.Visible;
+            //verberg dit
             this.Hide();
         }
     }
--- project Bomberman/Logic/Tile.cs
+++ project Bomberman/Logic/Tile.cs
@@ -9,136 +9,76 @@ namespace project_Bomberman
     //this class contains everything tile related
     class Tile
     {
-        public double posX = 0.0;                   //x positie van de tile middenpunt
-        public double posY = 0.0;                   //y positie van de tile middenpunt
+        public double posX = 0.0;                   //x positie van de tile 
+        public double posY = 0.0;                   //y positie van de tile 
 
         public double DebugPosX = 0;                //middenpunt van tile x
         public double DebugPosY = 0;                //middenpunt van tile y
-        public Brush OgPic = new ImageBrush();
-        public Brush BreakablePic = new ImageBrush();
-        public Timer ResetTimer = new Timer(500);
-        public string Type = "";
+        public Brush OgPic = new ImageBrush();      //orginele plaatje van de tile
+        public Brush BreakablePic = new ImageBrush();   //plaatje als de tile breekbaar is
+        public Timer ResetTimer = new Timer(500);       //De resettimer regelt tijd voordat de tile terug gaat naar de orginele staat
+        public string Type = "";                        //Het type dat de tile is bijvoorbeeld "breakable"
 
-        private bool exploding = false;
-        public bool Exploding
+        public bool Exploding = false;                  //ontploft deze tile
+        public bool TileSetup = false;                  //word de tile veranderd
+        public bool HasBomb = false;                    //bezit de tile een bom
+        public bool Passable = false;                   //Kan deze tile gepaseerd worden
+        public bool Hasplayer = false;                  //heeft de spler zijn bom als laatste op dezr tile geplaatst
+        public bool ResetDone = false;                  //is de tile klaar met zijn reset
+        private bool resetStarted = false;              //is de reset gestart
+        public bool ResetStarted                        //get set om reset te starten zodra de variabel is veranderd
         {
-            get
-            {
-                return exploding;
-            }
-            set
-            {
-                if (value)
-                {
-                    //Explode();
-                    exploding = value;
-
-                }
-                exploding = value;
-            }
-        }
-        public bool TileSetup = false;
-        public bool HasBomb = false;                //does the tile have a bomb
-        public bool Passable = false;
-        public bool Hasplayer = false;
-        public bool ResetDone = false;
-        private bool resetStarted = false;
-        public bool ResetStarted
-        {
+            //lever de info van de private resetStarted bool
             get
             {
                 return resetStarted;
             }
             set
             {
+                //als de nieuwe waarde true is
                 if (value)
                 {
+                    //start de cooldown
                     CooldownTile();
                 }
+                //zet de nieuwe waarde in de private bool
                 resetStarted = value;
             }
         }
-        public TextBox textBox;                     //een textbox
+ 
 
-        public float TileSize = 16;                 //hoe groot is de tile
+        public ImageBrush imageBrush = new ImageBrush();        //een image brush om plaatjes aan tiles toe te voegen
+        public Rectangle myRec = new Rectangle();               //de rectangle die deze tile vertegnwoordigt
 
-        public ImageBrush imageBrush = new ImageBrush();
-        public Rectangle myRec = new Rectangle();           //the rectangle in the class
-
-        //public Bomb Bomb;
+        //start de timer voor de reset van de tile
         public void CooldownTile()
         {
+            //roep functie aan als de resetimer interval is geweest
             ResetTimer.Elapsed += ResetTimer_Elapsed;
+            //start de resettimer
             ResetTimer.Start();
         }
-
+        //wordt aangeroepen als de timer een interval heeft gehad. hier wordt de tile gereset
         private void ResetTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
+            //de tile explodeert niet meer
             Exploding = false;
+            //de tile is klaar en wordt gereset
             ResetDone = true;
+            //Reset is klaar en geef dit aan
             resetStarted = false;
+            //stop de timer
             ResetTimer.Stop();
         }
-
-        //create a rectangle in the tileclass
-        public void CreateRect(int height, int width, ImageBrush img, double strokethickness)
-        {
-            myRec.Height = height;
-            myRec.Width = width;
-            myRec.Fill = img;
-            myRec.Stroke = Brushes.Black;
-            myRec.StrokeThickness = strokethickness;
-
-
-            //myRec.
-        }
-
-        //calculate the middlepoint of the tile
+        //bereken de middenpunt van de tile
         public void setVars()
         {
-            //calculate the middle y point
+            //pak het middelste punt van de y en zet hem naar debugPosY
             DebugPosY = posY + (myRec.Height / 2);
-            //calculate the middle x point
+            //pak het middelste punt van de x en zet hem naar debugPosX
             DebugPosX = posX + (myRec.Width / 2);
+            //Plaats het orginele plaatje in de tile
             OgPic = myRec.Fill;
-            //SetTextBlock();
         }
-
-        public void Explode()
-        {
-            ImageBrush Explodingpic = new ImageBrush();
-            Explodingpic.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/oke.png"));
-
-            //CreateRect(80, 80, Explodingpic, 2);
-            //Dispatcher.CurrentDispatcher.Invoke(()=>
-            //    {
-            //        myRec.Fill = Explodingpic;
-            //    });
-            //System.Windows.Application.Current.Dispatcher.Invoke(
-            //System.Windows.Threading.DispatcherPriority.Render, (Action)delegate
-            //{
-            //    myRec.Fill = Explodingpic;
-            //});
-            //myRec.Fill = Explodingpic;
-        }
-        //create a new textblock and enter the middle point in it
-        public void SetTextBlock()
-        {
-            textBox = new TextBox
-            {
-                Height = 50,
-                Width = 50,
-                Text = DebugPosX.ToString() + DebugPosY.ToString()
-            };
-        }
-
-
-
-        public void SetMyImage(BitmapImage img)
-        {
-            imageBrush.ImageSource = img;
-        }
-
-
     }
 }
